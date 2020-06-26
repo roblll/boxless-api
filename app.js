e277a86c96b2c85a95248023355a66231b543bc2
rob <lenon.rob@gmail.com>
2020-06-26 13:11:41 -0500

    Remove /api/dbtest and put new code to /api/vid

--- app.js
+++ app.js
@@ -22,63 +22,13 @@ const {
   getWeek,
 } = require("./utils/utils");
 
-let chartsState = {};
-
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/api/vid", async (req, res) => {
   try {
     const date = getRandDate(req.query);
     const week = getWeek(date);
-
     const chartName = getChartsSelected(req.query);
-
-    let chart = null;
-
-    if (chartsState[week]) {
-      console.log("found");
-      chart = chartsState[week];
-    } else {
-      console.log("not found");
-      chart = await getChart(chartName, week);
-      if (chart.length > 0) {
-        chartsState[week] = chart;
-      }
-    }
-
-    console.log(Object.keys(chartsState).length);
-
-    const songSearch = getSongSearch(chart, req.query);
-
-    const vid = await getSearchResult(songSearch);
-
-    if (vid) {
-      const { vidId, title, artist } = vid;
-      return res.json({ vidId, title, artist });
-    } else {
-      return res.json({});
-    }
-  } catch (e) {
-    return res.json({ error: e });
-  }
-});
-
-app.get("/api/searchvids", async (req, res) => {
-  try {
-    const searchTerm = req.query.search.replace(/%/g, " ");
-    const searchResults = await getSearchVids(searchTerm);
-    return res.json({ searchResults });
-  } catch (e) {
-    return res.json({ error: e });
-  }
-});
-
-app.get("/api/dbtest", async (req, res) => {
-  try {
-    const date = getRandDate(req.query);
-    const week = getWeek(date);
-    const chartName = getChartsSelected(req.query);
-    console.log(chartName);
 
     let chart = null;
 
@@ -106,6 +56,16 @@ app.get("/api/dbtest", async (req, res) => {
     } else {
       return res.json({});
     }
+  } catch (e) {
+    return res.json({ error: e });
+  }
+});
+
+app.get("/api/searchvids", async (req, res) => {
+  try {
+    const searchTerm = req.query.search.replace(/%/g, " ");
+    const searchResults = await getSearchVids(searchTerm);
+    return res.json({ searchResults });
   } catch (e) {
     return res.json({ error: e });
   }
