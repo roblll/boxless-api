d2c82ad8041d4677f7adc83ebbdf62b6f90b1d3f
rob <lenon.rob@gmail.com>
2020-10-03 12:36:10 -0500

    Add new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.077c37c3e64a27cfe0e5b30821b50785.js"
+  "/precache-manifest.5feb4e1be9e30f074588e1ea10c0a937.js"
 );
 
 self.addEventListener('message', (event) => {
