8ee19eced8d9cee88effeee89ffdcf169642c82a
rob <lenon.rob@gmail.com>
2022-03-13 16:53:42 -0500

    Update /db/index.js to remove ssl requirement for testing

--- db/index.js
+++ db/index.js
@@ -3,6 +3,9 @@ console.log("db", process.env.DATABASE_URL, "postgresql://localhost/boxless");
 const client = new Client({
   connectionString:
     process.env.DATABASE_URL || "postgresql://localhost/boxless",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 client.connect();
